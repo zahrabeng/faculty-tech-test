d5188c2f60bd178951a2334c56a7659622fe5e2e
Zahra <bghzahra@outlook.com>
2022-06-03 10:05:24 +0100

    added route to employee page and added employee id state

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { mergedData } from "./utils/Interfaces";
 
 import Main from "./components/Main";
 import ClientPage from "./components/ClientPage";
+import EmployeePage from "./components/EmployeePage";
 
 function App(): JSX.Element {
   const [clientId, setClientId] = useState<string>("");
+  const [employeeId, setEmployeeId] = useState<string>("");
   const [mergedData, setMergedData] = useState<mergedData | undefined>();
 
   console.log("This is merged", mergedData);
@@ -24,6 +26,11 @@ function App(): JSX.Element {
             path={`/clients/${clientId}`}
             element={<ClientPage clientId={clientId} mergedData={mergedData} />}
           ></Route>
+          <Route
+          path="/employees/"
+          element={<EmployeePage/>}
+          >
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/EmployeePage.tsx
@@ -0,0 +1,6 @@
+export default function EmployeePage():JSX.Element{
+    return(
+        <>
+        </>
+    )
+}
